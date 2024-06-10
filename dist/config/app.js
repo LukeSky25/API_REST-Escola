87d83ae2f15c11c1dae7fccedd83aab68f825134
LukeSky25 <lucasskywalkerodrigues@gmail.com>
2024-06-10 19:00:26 -0300

    Resolvendo erro de renderização de imagens denovo 3

--- dist/config/app.js
+++ dist/config/app.js
@@ -1,3 +1,5 @@
-"use strict";Object.defineProperty(exports, "__esModule", {value: true});exports. default = {
+"use strict";Object.defineProperty(exports, "__esModule", {value: true});require('dotenv').config();
+
+exports. default = {
   url: process.env.APP_URL
 };
