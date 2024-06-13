cb098a99b56902a52d4686d60996c80079768e13
LukeSky25 <lucasskywalkerodrigues@gmail.com>
2024-06-13 10:58:31 -0300

    resolvendo erro

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ import fotoRoutes from './routes/fotoRoutes';
 
 const whiteList = [
   'http://192.168.0.105:3001',
-  'http://localhost:3000'
+  'http://localhost:3001'
 ];
 
 const corsOptions = {
